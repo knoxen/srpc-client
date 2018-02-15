f20d52d7ec5f52ce1d4abbf69ce99328f98880a3
dingo sky <paul@dingosky.com>
2018-02-14 17:01:20 -0800

    Return list of current named connections

--- lib/srpc_client/connection_supervisor.ex
+++ lib/srpc_client/connection_supervisor.ex
@@ -4,12 +4,22 @@ defmodule SrpcClient.ConnectionSupervisor do
   use DynamicSupervisor
 
   def child_spec(_) do
-    %{id: __MODULE__,
-      start: {__MODULE__, :start_link, [[]]},
-      type: :supervisor}
+    %{id: __MODULE__, start: {__MODULE__, :start_link, [[]]}, type: :supervisor}
   end
 
   def start_link([]), do: DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
 
   def init([]), do: DynamicSupervisor.init(strategy: :one_for_one)
+
+  # def handle_call(:connections, _from, state)
+
+  def connections do
+    DynamicSupervisor.which_children(__MODULE__)
+    |> Enum.map(fn {_, conn, _, _} -> named_conn(conn) end)
+  end
+
+  defp named_conn(conn) do
+    %{name: name} = SrpcClient.info(conn)
+    {name, conn}
+  end
 end
